d188482986452d3f08718e5ad1d975e0514f2252
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-04-20 10:47:00 +0530

    little mistake

--- plugins/channel.py
+++ plugins/channel.py
@@ -79,7 +79,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         caption_message = f"#New_File_Added ✅\n\nFile_Name:- <code>{movie_name}</code>\n\nLanguage:- {language}\n\nQuality:- {quality}" 
         search_movie = movie_name.replace(" ", '-')
         movie_update_channel = await db.movies_update_channel_id()    
-        btn = [[
+        btn = [
             InlineKeyboardButton('📂 ɢᴇᴛ ғɪʟᴇ 📂', url=f'https://telegram.me/N2_Movies_Bot?start=getfile-{search_movie}')
         ]
         reply_markup = InlineKeyboardMarkup(btn)

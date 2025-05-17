d098f588f2684a801090fb5448f62284fa90cc4a
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-17 15:09:51 +0530

    try

--- plugins/commands.py
+++ plugins/commands.py
@@ -1133,3 +1133,18 @@ async def reset_group_command(client, message):
     await save_default_settings(grp_id)
     await message.reply_text('ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ʀᴇꜱᴇᴛ ɢʀᴏᴜᴘ ꜱᴇᴛᴛɪɴɢꜱ...')
     
+
+CMD = ["/", "."]
+
+@Client.on_message(filters.command("alive", CMD))
+async def check_alive(_, message):
+    await message.reply_text("**You are very lucky 🤞 I am alive ❤️ Press /start to use me**")
+
+
+@Client.on_message(filters.command("ping", CMD))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
